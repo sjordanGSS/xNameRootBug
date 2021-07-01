c44d157eff104595f577a97607e41c7d4c770a81
Sam Jordan <sjordan@gsspos.co.uk>
2021-07-01 13:51:18 +0100

    Add project files.

---
+++ xNameRootBug/xNameRootBug/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace xNameRootBug
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            collection.ItemsSource = new List<string>
+            {
+                string.Empty
+            };
+        }
+
+
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine($"clicked called from {this.GetType()}");
+        }
+    }
+}
